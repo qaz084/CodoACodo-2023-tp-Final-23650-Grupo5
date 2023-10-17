d0c1525014873691ac239c437256702225c173e1
qaz084 <jonathaniurman@gmail.com>
2023-10-17 16:07:34 -0300

    subiendo proyecto al repo

---
+++ src/main/java/com/tpfinal23650grupo5/TpFinal23650Grupo5Application.java
@@ -0,0 +1,13 @@
+package com.tpfinal23650grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinal23650Grupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinal23650Grupo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpfinal23650grupo5/TpFinal23650Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpfinal23650grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinal23650Grupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
